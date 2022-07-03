94a3b880e30235e2e7fa84b03cbf8968e0bc76ba
arunp3441 <arun.p3441@gmail.com>
2022-07-03 17:11:50 +0530

    added email list

--- src/main/java/io/arunp/inbox/InboxApp.java
+++ src/main/java/io/arunp/inbox/InboxApp.java
@@ -1,7 +1,12 @@
 package io.arunp.inbox;
 
 import java.nio.file.Path;
+import java.util.Arrays;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import io.arunp.inbox.emailslist.EmailListItem;
+import io.arunp.inbox.emailslist.EmailListItemKey;
+import io.arunp.inbox.emailslist.EmailListItemRepository;
 import io.arunp.inbox.folders.Folder;
 import io.arunp.inbox.folders.FolderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,9 @@ public class InboxApp {
 	@Autowired
 	private FolderRepository folderRepository;
 
+	@Autowired
+	private EmailListItemRepository emailListItemRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(InboxApp.class, args);
 	}
@@ -35,5 +43,19 @@ public class InboxApp {
 		folderRepository.save(new Folder("arunp3441","Inbox","blue"));
 		folderRepository.save(new Folder("arunp3441","Sent","green"));
 		folderRepository.save(new Folder("arunp3441","Important","yellow"));
+		for(int i=0; i<=10;++i){
+			EmailListItemKey key = new EmailListItemKey();
+			key.setUserId("arunp3441");
+			key.setLabel("Inbox");
+			key.setTimeUUID(Uuids.timeBased());
+
+			EmailListItem item = new EmailListItem();
+			item.setKey(key);
+			item.setTo(Arrays.asList("arunpp26"));
+			item.setSubject("Subject "+i);
+			item.setUnread(true);
+
+			emailListItemRepository.save(item);
+		}
 	}
 }
--- src/main/java/io/arunp/inbox/controllers/InboxController.java
+++ src/main/java/io/arunp/inbox/controllers/InboxController.java
@@ -1,8 +1,12 @@
 package io.arunp.inbox.controllers;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import io.arunp.inbox.emailslist.EmailListItem;
+import io.arunp.inbox.emailslist.EmailListItemRepository;
 import io.arunp.inbox.folders.Folder;
 import io.arunp.inbox.folders.FolderRepository;
 import io.arunp.inbox.folders.FolderService;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -11,7 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class InboxController {
@@ -22,15 +28,33 @@ public class InboxController {
     @Autowired
     private FolderService folderService;
 
+    @Autowired
+    private EmailListItemRepository emailListItemRepository;
+
     @GetMapping(value = "/")
     public String homePage(@AuthenticationPrincipal OAuth2User principal, Model model){
         if(principal ==  null || !StringUtils.hasText(principal.getAttribute("login")))
             return "index";
         String userId = principal.getAttribute("login");
+
+        // Fetch Folders
         List<Folder> userFolders = folderRepository.findAllByUserId(userId);
         model.addAttribute("userFolders" , userFolders);
         List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
         model.addAttribute("defaultFolders" , defaultFolders);
+
+        //Fetch emails
+
+        List<EmailListItem> emailList = emailListItemRepository.findAllByKey_UserIdAndKey_Label(userId,"Inbox");
+        model.addAttribute("emailList" , emailList);
+
+        PrettyTime p = new PrettyTime();
+        emailList.stream().forEach(email -> {
+            UUID timeUUID = email.getKey().getTimeUUID();
+            Date emailDateTime = new Date(Uuids.unixTimestamp(timeUUID));
+            email.setAgoTimeString(p.format(emailDateTime));
+        });
+
         return "inbox-page";
     }
 }
